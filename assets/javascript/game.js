5877cde8bcc32b2a9a134d181b38f23a2e80759c
Howard Daniels <hdaniels13tat@gmail.com>
2019-11-30 01:53:36 -0500

    developed code for assigning points to jewels

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,23 @@
-document.getElementById("score").innerHTML = Math.floor(Math.random() * 100);
+var random = Math.floor(Math.random() * 100);
+document.getElementById("score").innerHTML = random;
+
+if (random < 4) {
+    ruby = 0;
+    beryl = 1;
+    emerald = 5;
+    sapphire = 10;
+}
+
+else if (random < 8 && random > 3) {
+    ruby = 0;
+    beryl = 1;
+    emerald = 10;
+    sapphire = 5;
+}
+
+else if (random < 12 && random > 7) {
+    ruby = 0;
+    beryl = 10;
+    emerald = 1;
+    sapphire = 5;
+}
